b0885c932a3dc1f192c2c7c7f28bd34e18b96219
gushimaaceline <celinegushimaa@gmail.com>
2021-04-02 00:21:30 +0300

    first commit

---
+++ src/Assignment1_Kotlin.kt
@@ -0,0 +1,18 @@
+import java.util.*
+
+fun main() {
+    var fullName= "Gushima Celine"
+    println(fullName)
+    var age= 22
+    println(age)
+    var phoneNumber= "0781405420"
+    println(phoneNumber)
+    var weight= 59.0
+    println(weight)
+    var citizen= "Kenya"
+    println(citizen)
+    var kenyanCitizen: Boolean= false
+    println(kenyanCitizen)
+    var name= arrayOf ("Gushima Celine", 22,"0781405420",59, "Kenya", false)
+    println(Arrays.toString(name))
+}
